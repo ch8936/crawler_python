0579378e0f83049e8687472f143f282d10fb6b6a
Cheng Hui <chenghuiaz@gmail.com>
2017-12-07 01:06:47 +0800

    Update basic.py

--- src/basic.py
+++ src/basic.py
@@ -377,4 +377,217 @@ import requests
 response = requests.get('http://www.jianshu.com')
 exit() if not response.status_code == 200 else print('Request Successfully')
 
+requests内置的状态字符
+100: ('continue',),
+101: ('switching_protocols',),
+102: ('processing',),
+103: ('checkpoint',),
+122: ('uri_too_long', 'request_uri_too_long'),
+200: ('ok', 'okay', 'all_ok', 'all_okay', 'all_good', '\\o/', '✓'),
+201: ('created',),
+202: ('accepted',),
+203: ('non_authoritative_info', 'non_authoritative_information'),
+204: ('no_content',),
+205: ('reset_content', 'reset'),
+206: ('partial_content', 'partial'),
+207: ('multi_status', 'multiple_status', 'multi_stati', 'multiple_stati'),
+208: ('already_reported',),
+226: ('im_used',),
+
+# Redirection.
+300: ('multiple_choices',),
+301: ('moved_permanently', 'moved', '\\o-'),
+302: ('found',),
+303: ('see_other', 'other'),
+304: ('not_modified',),
+305: ('use_proxy',),
+306: ('switch_proxy',),
+307: ('temporary_redirect', 'temporary_moved', 'temporary'),
+308: ('permanent_redirect',
+      'resume_incomplete', 'resume',), # These 2 to be removed in 3.0
+
+# Client Error.
+400: ('bad_request', 'bad'),
+401: ('unauthorized',),
+402: ('payment_required', 'payment'),
+403: ('forbidden',),
+404: ('not_found', '-o-'),
+405: ('method_not_allowed', 'not_allowed'),
+406: ('not_acceptable',),
+407: ('proxy_authentication_required', 'proxy_auth', 'proxy_authentication'),
+408: ('request_timeout', 'timeout'),
+409: ('conflict',),
+410: ('gone',),
+411: ('length_required',),
+412: ('precondition_failed', 'precondition'),
+413: ('request_entity_too_large',),
+414: ('request_uri_too_large',),
+415: ('unsupported_media_type', 'unsupported_media', 'media_type'),
+416: ('requested_range_not_satisfiable', 'requested_range', 'range_not_satisfiable'),
+417: ('expectation_failed',),
+418: ('im_a_teapot', 'teapot', 'i_am_a_teapot'),
+421: ('misdirected_request',),
+422: ('unprocessable_entity', 'unprocessable'),
+423: ('locked',),
+424: ('failed_dependency', 'dependency'),
+425: ('unordered_collection', 'unordered'),
+426: ('upgrade_required', 'upgrade'),
+428: ('precondition_required', 'precondition'),
+429: ('too_many_requests', 'too_many'),
+431: ('header_fields_too_large', 'fields_too_large'),
+444: ('no_response', 'none'),
+449: ('retry_with', 'retry'),
+450: ('blocked_by_windows_parental_controls', 'parental_controls'),
+451: ('unavailable_for_legal_reasons', 'legal_reasons'),
+499: ('client_closed_request',),
+
+# Server Error.
+500: ('internal_server_error', 'server_error', '/o\\', '✗'),
+501: ('not_implemented',),
+502: ('bad_gateway',),
+503: ('service_unavailable', 'unavailable'),
+504: ('gateway_timeout',),
+505: ('http_version_not_supported', 'http_version'),
+506: ('variant_also_negotiates',),
+507: ('insufficient_storage',),
+509: ('bandwidth_limit_exceeded', 'bandwidth'),
+510: ('not_extended',),
+511: ('network_authentication_required', 'network_auth', 'network_authentication'),
+
+高级操作
+文件上传
+import requests
+files = {'file':open('favicon.ico','rb')}
+response = requests.post("http://httpbin.org/post",files=files)
+print(response.text)
+
+获取cookie
+import requests
+response = requests.get("https://www.baidu.com")
+print(response.cookies)
+for key,value in response.cookies.items():
+	print(key + '=' + value)
+
+会话维持
+模拟登陆
+import requests
+requests.get("http://httpbin.org/cookies/set/number/123456789")
+response = requests.get("http://httpbin.org/cookies")
+print(response.text)
+
+import requests
+s = requests.Session()
+s.get("http://httpbin.org/cookies/set/number/123456789")
+response = s.get("http://httpbin.org/cookies")
+print(response.text)
+
+证书验证
+import requests
+response = requests.get('https://www.12306.cn')
+print(response.status_code)
+
+import requests
+from requests.packages import urllib3
+urllib3.disable_warnings()
+response = requests.get('https://www.12306.cn',verify=False)
+print(response.status_code)
+
+import requests
+response = requests.get('http://www.12306.cn',cert=('/path/server.crt','/path/key'))
+print(response.status_code)
+
+代理设置
+import requests
+proxies={
+	'http':'http://127.0.0.1:9743',
+	'https':'https://127.0.0.1:9743'
+}
+response = requests.get('https://www.taobao.com',proxies=proxies)
+print(response.status_code)
+
+import requests
+proxies={
+	'http':'http://user:password@127.0.0.1:9743/'
+}
+response = requests.get('https://www.taobao.com',proxies=proxies)
+print(response.status_code)
+
+pip3 install 'requests[socks]'
+
+import requests
+proxies={
+	'http':'socks5://127.0.0.1:9743',
+	'https':'socks5://127.0.0.1:9743'
+}
+response = requests.get('https://www.taobao.com',proxies=proxies)
+print(response.status_code)
+
+超时设置
+import requests
+from requests.exceptions import ReadTimeout
+try:
+    # 设置必须在500ms内收到响应，不然或抛出ReadTimeout异常
+    response = requests.get("http://httpbin.org/get", timeout=0.5)
+    print(response.status_code)
+except ReadTimeout:
+    print('Timeout')
+
+认证设置
+import requests
+from requests.auth import HTTPBasicAuth
+r = requests.get('http://120.27.34.24:9001', auth=HTTPBasicAuth('user', '123'))
+# r = requests.get('http://120.27.34.24:9001', auth=('user', '123'))
+print(r.status_code)
+
+异常处理
+import requests
+from requests.exceptions import ReadTimeout, ConnectionError, RequestException
+try:
+    response = requests.get("http://httpbin.org/get", timeout = 0.5)
+    print(response.status_code)
+except ReadTimeout:
+    print('Timeout')
+except ConnectionError:
+    print('Connection error')
+except RequestException:
+    print('Error')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
