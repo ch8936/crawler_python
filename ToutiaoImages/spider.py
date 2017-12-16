a1bba88c31c57e0f6377b355a2b22cc9b17cf3bb
Cheng Hui <chenghuiaz@gmail.com>
2017-12-16 23:57:30 +0800

    Update spider.py

--- ToutiaoImages/spider.py
+++ ToutiaoImages/spider.py
@@ -1,1 +1,90 @@
+import json
+import os
+from urllib.parse import urlencode
+import pymongo
+import requests
+from bs4 import BeautifulSoup
+from requests.exceptions import RequestException
+import re
+from multiprocessing import Pool
+from hashlib import md5
+from json.decoder import JSONDecodeError
+from config import *
 
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB]
+
+def get_page_index(offset, keyword):
+    data = {
+        'offset':offset,
+        'format': 'json',
+        'keyword': keyword,
+        'autoload': 'true',
+        'count': '20',
+        'cur_tab': 3,
+        'from': 'gallery'
+    }
+    url = 'http://www.toutiao.com/search_content/?' + urlencode(data)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        print('请求索引页出错')
+        return None
+
+def parse_page_index(html):
+    data = json.loads(html)
+    if data and 'data' in data.keys():
+        for item in data.get('data'):
+            yield item.get('article_url')
+
+def get_page_detail(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        print('请求详情页出错')
+        return None
+
+def parse_page_detail(html,url):
+    soup = BeautifulSoup(html,'lxml')
+    title = soup.select('title')[0].get_text()
+    print(title)
+    images_pattern = re.compile('gallery: (.*),[\s\S]*?siblingList:', re.S)
+    result = re.search(images_pattern, html)
+    if result:
+        images_url_1 = result.group(1).replace('\\"', '"')
+        images_url_2 = images_url_1.replace('\\\\', '\\')
+        images_url_3 = images_url_2.replace('\/', '/')
+        images_url_4 = images_url_3.replace('JSON.parse("', '')
+        images_url_5 = images_url_4.replace('")', '')
+        data = json.loads(images_url_5)
+        if data and 'sub_images' in data.keys():
+            sub_images = data.get('sub_images')
+            images = [item.get('url') for item in sub_images]
+            return {
+                'title':title,
+                'url':url,
+                'images':images
+            }
+
+def save_to_mongo(result):
+    if db[MONGO_TABLE].insert(result):
+        print('存储到MongoDB成功', result)
+        return True
+    return False
+
+def main():
+    html = get_page_index(0, '街拍')
+    for url in parse_page_index(html):
+        html = get_page_detail(url)
+        if html:
+            result = parse_page_detail(html, url)
+            save_to_mongo(result)
+
+if __name__ == '__main__':
+    main()
