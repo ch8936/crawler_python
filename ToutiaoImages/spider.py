bad18ad0060e59b7fdd8991883ccd0bd50514c37
Cheng Hui <chenghuiaz@gmail.com>
2017-12-17 00:45:14 +0800

    Update spider.py

--- ToutiaoImages/spider.py
+++ ToutiaoImages/spider.py
@@ -11,7 +11,7 @@ from hashlib import md5
 from json.decoder import JSONDecodeError
 from config import *
 
-client = pymongo.MongoClient(MONGO_URL)
+client = pymongo.MongoClient(MONGO_URL, connect=False)
 db = client[MONGO_DB]
 
 def get_page_index(offset, keyword):
@@ -63,14 +63,34 @@ def parse_page_detail(html,url):
         images_url_4 = images_url_3.replace('JSON.parse("', '')
         images_url_5 = images_url_4.replace('")', '')
         data = json.loads(images_url_5)
+#        print(images_url_3)
         if data and 'sub_images' in data.keys():
             sub_images = data.get('sub_images')
             images = [item.get('url') for item in sub_images]
+            for image in images: download_image(image)
             return {
-                'title':title,
-                'url':url,
-                'images':images
+                'title': title,
+                'url': url,
+                'images': images
             }
+
+def download_image(url):
+    print('正在下载', url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            save_image(response.content)
+        return None
+    except RequestException:
+        print('请求图片出错', url)
+        return None
+
+def save_image(content):
+    file_path = '{0}/{1}.{2}'.format(os.getcwd(), md5(content).hexdigest(), 'jpg')
+    if not os.path.exists(file_path):
+        with open(file_path, 'wb') as f:
+            f.write(content)
+            f.close()
 
 def save_to_mongo(result):
     if db[MONGO_TABLE].insert(result):
@@ -78,13 +98,15 @@ def save_to_mongo(result):
         return True
     return False
 
-def main():
-    html = get_page_index(0, '街拍')
+def main(offset):
+    html = get_page_index(offset, KEYWORD)
     for url in parse_page_index(html):
         html = get_page_detail(url)
         if html:
             result = parse_page_detail(html, url)
-            save_to_mongo(result)
+            if result: save_to_mongo(result)
 
 if __name__ == '__main__':
-    main()
+    groups = [x*20 for x in range(GROUP_START, GROUP_END + 1)]
+    pool = Pool()
+    pool.map(main, groups)
